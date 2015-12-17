745f9f943008c826b7f3011c3ad1ca5c8e2db451
Laurent Direr <laurent.direr@gmail.com>
2015-12-17 18:38:19 +0700

    Adding icon pngs. Credit to https://github.com/chrisolsen/football-scores-svgs.

--- app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
@@ -1,12 +1,16 @@
 package barqsoft.footballscores.widget;
 
+import android.annotation.TargetApi;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.RemoteViews;
@@ -15,7 +19,9 @@ import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.R;
 import barqsoft.footballscores.Utilies;
 import barqsoft.footballscores.ViewHolder;
+import barqsoft.footballscores.service.myFetchService;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class ScoresWidgetProvider extends AppWidgetProvider {
 
     private final static String[] SCORES_COLUMNS = {
@@ -48,10 +54,49 @@ public class ScoresWidgetProvider extends AppWidgetProvider {
         Log.d(LOG_TAG, "in onUpdate");
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-            views.setRemoteAdapter(R.id.widget_list, new Intent(context, ScoresWidgetRemoteViewsService.class));
+            // Set up the collection
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if(myFetchService.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            // A ComponentName instance is an identifier for a specific application component.
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        }
+    }
+
+    // Copied from advanced android development.
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list, new Intent(context, ScoresWidgetRemoteViewsService.class));
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list, new Intent(context, ScoresWidgetRemoteViewsService.class));
+    }
 }
+
